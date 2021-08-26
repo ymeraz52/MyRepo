cdae627ca84c0814651e75cc1a156583f5533f89
ymeraz52 <ymeraz52@gmail.com>
2021-08-26 14:13:46 -0400

    in class activity

---
+++ practice.py
@@ -0,0 +1,8 @@
+num = int(input("What is your number? "))
+if num == 10:
+    print("YOU ARE CORRECT!")
+elif num < 10:
+    print("You are low!")
+elif num > 10:
+    print("You are high!")
+print("Goodbye!")
