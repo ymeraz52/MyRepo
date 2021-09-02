74656efb3a699484d7f751b7855794bd41f0370c
ymeraz52 <ymeraz52@gmail.com>
2021-09-02 15:25:37 -0400

    Homework

---
+++ module.py
@@ -0,0 +1,28 @@
+def is_num():
+    while True:
+        try:
+            num = int(input("Give me a number: "))
+            break
+        except ValueError:
+            print("This is not a number")
+            continue
+    return num
+my_list = []
+my_list.append(is_num())
+my_list.append(is_num())
+my_list.append(is_num())
+my_list.append(4)
+my_list.append(5)
+my_list.append(6)
+my_list.append(7)
+my_list.append("Apple")
+my_list.append("Potato")
+
+#length = len(my_list)
+#count = 0
+#while count < length:
+#    print(my_list[count])
+#   count +=1
+
+for i in my_list:
+    print(i)
